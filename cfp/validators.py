58108f1f7ae3218eecbb8367a068f5f6d2a0f218
Kirk Northrop <kirk@krn.me.uk>
2018-04-12 21:19:20 +0100

    “Although practicality beats purity.”

--- cfp/validators.py
+++ cfp/validators.py
@@ -1,14 +1,7 @@
 from django.core.exceptions import ValidationError
 
 
-def validate_max_words(max_words):
-    def validator(value):
-        num_words = len(value.split())
-        if num_words > max_words:
-            raise ValidationError(f'Field is too long: {num_words} words / {max_words} limit')
-    return validator
-
-
-#  https://code.djangoproject.com/ticket/28346
-validate_max_300_words = validate_max_words(300)
-validate_max_300_words.__name__ = 'validate_max_300_words'
+def validate_max_300_words(value):
+    num_words = len(value.split())
+    if num_words > 300:
+        raise ValidationError(f'Field is too long: {num_words}')
