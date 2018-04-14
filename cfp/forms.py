d6333897364eab7d231e1fab114eb5603bfa957b
Kirk Northrop <kirk@krn.me.uk>
2018-04-14 11:37:07 +0100

    The return of description_private

--- cfp/forms.py
+++ cfp/forms.py
@@ -14,6 +14,7 @@ class ProposalForm(forms.ModelForm):
             'subtitle',
             'copresenter_names',
             'description',
+            'description_private',
             'outline',
             'aimed_at_new_programmers',
             'aimed_at_teachers',
@@ -28,6 +29,7 @@ class ProposalForm(forms.ModelForm):
             'session_type': 'What are you proposing?',
             'copresenter_names': 'Are you presenting with anybody else?',
             'description': 'What is your session about?',
+            'description_private': 'Is there anything else we should know about your proposal?',
             'outline': 'Can you give us an outline of your proposed session?',
             'aimed_at_new_programmers': 'new programmers?',
             'aimed_at_teachers': 'teachers?',
@@ -42,6 +44,7 @@ class ProposalForm(forms.ModelForm):
             'copresenter_names': 'If you are presenting with anybody else, please list their names here.',
             'description': 'If your session is selected, this is the basis of what will be published in the programme.  Limit: 300 words.',
             'outline': 'An outline of your session is optional, but helps the programme committee. A proposal with an outline is more likely to be selected than one without. More detail, including timings, is better. The outline will not be published. Limit: 300 words.',
+            'description_private': 'Your answer here is for the benefit of the programme committee, and will not be published. Limit: 300 words.',
         }
 
         widgets = {
@@ -49,6 +52,7 @@ class ProposalForm(forms.ModelForm):
             'subtitle': forms.TextInput(attrs={'placeholder': False}),
             'copresenter_names': forms.Textarea(attrs={'cols': 40, 'rows': 3, 'placeholder': False}),
             'description': forms.Textarea(attrs={'placeholder': False}),
+            'description_private': forms.Textarea(attrs={'placeholder': False}),
             'outline': forms.Textarea(attrs={'placeholder': False}),
         }
 
