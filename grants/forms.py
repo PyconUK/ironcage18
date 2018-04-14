4921c742a995bc514525f758469196dc8de0db64
Kirk Northrop <kirk@krn.me.uk>
2018-04-14 16:34:26 +0100

    Changes to Grants questions

--- grants/forms.py
+++ grants/forms.py
@@ -18,30 +18,41 @@ class ApplicationForm(forms.ModelForm):
     class Meta:
         model = Application
         fields = [
-            'amount_requested',
-            'would_like_ticket_set_aside',
             'about_you',
+            'about_why',
+            'requested_ticket_only',
+            'amount_requested',
+            'cost_breakdown',
         ]
 
         labels = {
-            'amount_requested': 'How much are you asking for?',
-            'would_like_ticket_set_aside': 'Would you like a ticket set aside for you until your grant is allocated?',
-            'about_you': 'Tell us a little bit about yourself!',
+            'about_you': 'Tell us a little bit about yourself',
+            'about_why': 'Tell us how you feel attending PyCon UK will be of value',
+            'amount_requested': 'What amount in financial assistance would make it possible for you to attend the conference without hardship?',
+            'cost_breakdown': 'Please break down the costs you anticipate for travel, accommodation and daily expenses',
         }
 
         help_texts = {
-            'amount_requested': 'Please give the amount of money (in GBP) you would require to attend the conference',
-            'would_like_ticket_set_aside': 'If you cannot afford a ticket without assistance, we will reserve a ticket until your application is processed',
-            'about_you': 'Let us know just a little bit about why attending PyCon UK would be important to you. Feel free to elaborate a little bit about who you are and what you do, as well.',
+            'about_you': 'Introduce yourself - tell us about what you do, where you come from and so on. Required.',
+            'about_why': 'This can include its value to you, the conference itself, the wider Python community, your own society, or anything at all. Required.',
+            'amount_requested': 'Please ensure you specify the currency (preferably in Sterling). Required.',
+            'cost_breakdown': 'Please see our <a href="https://2018.pyconuk.org/travel-accommodation" target="_blank">travel and accommodation guidance pages</a> to help calculate your costs. Required.'
         }
 
         widgets = {
-            'amount_requested': forms.NumberInput(attrs={'placeholder': False}),
             'about_you': forms.Textarea(attrs={'placeholder': False}),
+            'about_why': forms.Textarea(attrs={'placeholder': False}),
+            'amount_requested': forms.TextInput(attrs={'placeholder': False}),
+            'cost_breakdown': forms.Textarea(attrs={'placeholder': False}),
         }
 
+    requested_ticket_only = forms.ChoiceField(
+        choices=Application.REQUESTED_TICKET_ONLY_CHOICES,
+        label='Do you need further finanical assistance?'
+    )
+
     days = forms.MultipleChoiceField(
-        required=False,
+        required=True,
         choices=DAY_CHOICES,
         widget=ButtonsCheckbox
     )
