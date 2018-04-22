8a8905257d500175192447f7cbf01f5c88003dcd
Kirk Northrop <kirk@krn.me.uk>
2018-04-22 16:31:26 +0100

    Improve ticket and order admins

--- orders/admin.py
+++ orders/admin.py
@@ -2,12 +2,33 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
-from orders.models import Order
+from orders.models import Order, OrderRow
 # from tickets.actions import refund_order
 
 from import_export.admin import ExportMixin
 from django_object_actions import DjangoObjectActions
 from tickets.admin import OurActionsOnlyMixin
+
+
+class OrderRowInline(OurActionsOnlyMixin, admin.TabularInline):
+    model = OrderRow
+
+    fields = readonly_fields = ('link_to_item', 'item_descr',
+                                'item_descr_extra', 'row_cost_inc_vat')
+
+    def link_to_item(self, obj):
+        url = reverse(
+            f'admin:{obj.content_type.app_label}_{obj.content_type}_change',
+            args=[obj.object_id]
+        )
+        return format_html("<a href='{}'>{}: {}</a>", url,
+                           str(obj.content_type).capitalize(), obj.item)
+    link_to_item.admin_order_field = 'item'
+    link_to_item.short_description = 'item'
+
+    def row_cost_inc_vat(self, obj):
+        if obj.cost_excl_vat:
+            return f'£{obj.cost_incl_vat}'
 
 
 @admin.register(Order)
@@ -28,16 +49,32 @@ class OrderAdmin(DjangoObjectActions, OurActionsOnlyMixin, admin.ModelAdmin):
         return actions
 
     view_on_site = False
-    fields = ('link_to_purchaser', 'invoice_number', 'status', 'stripe_charge_id', 'stripe_charge_created',
+    fields = ('link_to_purchaser', 'invoice_number', 'status',
+              'stripe_charge_id', 'stripe_charge_created',
               'stripe_charge_failure_reason', 'billing_name', 'billing_addr')
-    readonly_fields = ('link_to_purchaser', 'invoice_number', 'status', 'stripe_charge_id',
-                       'stripe_charge_created', 'stripe_charge_failure_reason')
-    list_display = ('order_id', 'link_to_purchaser', 'status')
+    readonly_fields = fields
+
+    list_display = ('order_id', 'link_to_purchaser', 'purchaser_email',
+                    'num_tickets', 'order_cost', 'status')
+
     list_filter = ('status', )
+
     search_fields = ['purchaser__name']
+
+    inlines = [
+        OrderRowInline,
+    ]
 
     def link_to_purchaser(self, obj):
         url = reverse('admin:accounts_user_change', args=[obj.purchaser.id])
         return format_html("<a href='{}'>{}</a>", url, obj.purchaser.name)
-    link_to_purchaser.admin_order_field = 'purchaser'
+    link_to_purchaser.admin_order_field = 'purchaser__name'
     link_to_purchaser.short_description = 'purchaser'
+
+    def purchaser_email(self, obj):
+        return obj.purchaser.email_addr
+    purchaser_email.admin_order_field = 'purchaser__email_addr'
+    purchaser_email.short_description = 'email'
+
+    def order_cost(self, obj):
+        return '£{:.2f}'.format(obj.cost_pence_incl_vat / 100)
--- tickets/admin.py
+++ tickets/admin.py
@@ -26,18 +26,27 @@ class OurActionsOnlyMixin():
 class TicketAdmin(DjangoObjectActions, OurActionsOnlyMixin, admin.ModelAdmin):
 
     view_on_site = False
-    fields = readonly_fields = ('ticket_id', 'link_to_owner', 'link_to_order', 'sat', 'sun', 'mon', 'tue', 'wed')
-    list_display = ('ticket_id', 'link_to_owner', 'link_to_order', 'sat', 'sun', 'mon', 'tue', 'wed')
-    list_filter = ('sat', 'sun', 'mon', 'tue', 'wed')
+    fields = readonly_fields = ('ticket_id', 'link_to_owner', 'link_to_order',
+                                'sat', 'sun', 'mon', 'tue', 'wed')
+    list_display = ('ticket_id', 'link_to_owner', 'link_to_order',
+                    'order_status', 'sat', 'sun', 'mon', 'tue', 'wed')
+    list_filter = ('order_rows__order__status', 'sat', 'sun', 'mon', 'tue', 'wed')
     search_fields = ['owner__name']
 
     def link_to_owner(self, obj):
-        url = reverse('admin:accounts_user_change', args=[obj.owner.id])
-        return format_html("<a href='{}'>{}</a>", url, obj.owner.name)
+        if obj.owner:
+            url = reverse('admin:accounts_user_change', args=[obj.owner.id])
+            return format_html("<a href='{}'>{}</a>", url, obj.owner.name)
+        else:
+            return None
     link_to_owner.admin_order_field = 'owner'
     link_to_owner.short_description = 'owner'
 
     def link_to_order(self, obj):
         url = reverse('admin:orders_order_change', args=[obj.order.id])
-        return format_html("<a href='{}'>{}</a>", url, obj.order.id)
+        return format_html("<a href='{}'>{}</a>", url, obj.order.order_id)
     link_to_order.admin_order_field = 'order'
+    link_to_order.short_description = 'order'
+
+    def order_status(self, obj):
+        return obj.order.status
