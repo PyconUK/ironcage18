355676432da3f2592f4f2ae80bb9f7f8cc8a00dd
Kirk Northrop <kirk@krn.me.uk>
2018-04-28 09:49:59 +0100

    Orders admin with standard permissions

--- orders/admin.py
+++ orders/admin.py
@@ -7,14 +7,21 @@ from orders.models import Order, OrderRow
 
 from import_export.admin import ExportMixin
 from django_object_actions import DjangoObjectActions
-from tickets.admin import OurActionsOnlyMixin
+from ironcage.admin import OurActionsOnlyMixin
 
 
 class OrderRowInline(OurActionsOnlyMixin, admin.TabularInline):
     model = OrderRow
 
-    fields = readonly_fields = ('link_to_item', 'item_descr',
-                                'item_descr_extra', 'row_cost_inc_vat')
+    def get_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return ['link_to_item', 'item_descr', 'item_descr_extra',
+                    'row_cost_inc_vat']
+        else:
+            return ['link_to_item', 'item_descr', 'item_descr_extra']
+
+    def get_readonly_fields(self, request, obj=None):
+        return self.get_fields(request, obj)
 
     def link_to_item(self, obj):
         url = reverse(
@@ -34,28 +41,21 @@ class OrderRowInline(OurActionsOnlyMixin, admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(DjangoObjectActions, OurActionsOnlyMixin, admin.ModelAdmin):
 
-    # def refund(self, request, obj):
-    #     refund_order(obj)
+    # # def refund(self, request, obj):
+    # #     refund_order(obj)
 
 
-    def get_change_actions(self, request, object_id, form_url):
-        actions = super().get_change_actions(request, object_id, form_url)
-        actions = list(actions)
+    # def get_change_actions(self, request, object_id, form_url):
+    #     actions = super().get_change_actions(request, object_id, form_url)
+    #     actions = list(actions)
 
-        obj = self.model.objects.get(pk=object_id)
-        # if obj.status == 'successful':
-        #     actions.append('refund')
+    #     obj = self.model.objects.get(pk=object_id)
+    #     # if obj.status == 'successful':
+    #     #     actions.append('refund')
 
-        return actions
+    #     return actions
 
     view_on_site = False
-    fields = ('link_to_purchaser', 'invoice_number', 'status',
-              'stripe_charge_id', 'stripe_charge_created',
-              'stripe_charge_failure_reason', 'billing_name', 'billing_addr')
-    readonly_fields = fields
-
-    list_display = ('order_id', 'link_to_purchaser', 'purchaser_email',
-                    'num_tickets', 'order_cost', 'status')
 
     list_filter = ('status', )
 
@@ -64,6 +64,27 @@ class OrderAdmin(DjangoObjectActions, OurActionsOnlyMixin, admin.ModelAdmin):
     inlines = [
         OrderRowInline,
     ]
+
+    def get_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return ['link_to_purchaser', 'invoice_number', 'status',
+                    'stripe_charge_id', 'stripe_charge_created',
+                    'stripe_charge_failure_reason', 'billing_name',
+                    'billing_addr']
+        else:
+            return ['link_to_purchaser', 'invoice_number', 'status']
+
+    def get_readonly_fields(self, request, obj=None):
+        return self.get_fields(request, obj)
+
+    def get_list_display(self, request):
+
+        if request.user.is_superuser:
+            return ['order_id', 'link_to_purchaser',
+                    'num_tickets', 'order_cost', 'status']
+        else:
+            return ['order_id', 'link_to_purchaser', 'num_tickets',
+                    'status']
 
     def link_to_purchaser(self, obj):
         url = reverse('admin:accounts_user_change', args=[obj.purchaser.id])
