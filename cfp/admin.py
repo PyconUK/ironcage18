d6cb190bf6dd07f087f3b7473bc12b10d4df6ad3
Kirk Northrop <kirk.northrop@novastonemedia.com>
2018-06-18 12:35:44 +0100

    Fix reviewer admin

--- cfp/admin.py
+++ cfp/admin.py
@@ -10,7 +10,11 @@ class ProposalAdmin(OurActionsOnlyMixin, admin.ModelAdmin):
                      'description_private', 'outline']
 
     def get_fields(self, request, obj=None):
-        if request.user.is_superuser:
+        users_permissions = request.user.get_all_permissions()
+
+        if (request.user.is_superuser
+                or 'cfp.review_education_proposal' in users_permissions   # noqa: W503
+                or 'cfp.review_proposal' in users_permissions):  # noqa: W503
             return ['id', 'proposer_name', 'session_type', 'title', 'subtitle',
                     'copresenter_names', 'description', 'description_private',
                     'outline', 'aimed_at_new_programmers', 'aimed_at_teachers',
@@ -27,10 +31,15 @@ class ProposalAdmin(OurActionsOnlyMixin, admin.ModelAdmin):
         users_permissions = request.user.get_all_permissions()
         if ('cfp.review_proposal' in users_permissions
                 or 'cfp.review_education_proposal' in users_permissions):  # noqa: W503
-            fields.remove('aimed_at_new_programmers')
-            fields.remove('aimed_at_teachers')
-            fields.remove('aimed_at_data_scientists')
-            fields.remove('state')
+            if 'aimed_at_new_programmers' in fields:
+                fields.remove('aimed_at_new_programmers')
+            if 'aimed_at_teachers' in fields:
+                fields.remove('aimed_at_teachers')
+            if 'aimed_at_data_scientists' in fields:
+                fields.remove('aimed_at_data_scientists')
+            if 'state' in fields:
+                fields.remove('state')
+
             fields.remove('special_reply_required')
 
         return fields
