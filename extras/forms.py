e0dcc350b7ff9ca2946571347b70b31849099da3
Kirk Northrop <kirk@krn.me.uk>
2018-08-29 21:29:34 +0100

    Add most of dinners

--- extras/forms.py
+++ extras/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
-from .models import ChildrenTicket
+from .models import (CITY_HALL_DESSERTS, CITY_HALL_DINNERS, CITY_HALL_MAINS,
+                     CITY_HALL_STARTERS, CLINK_DESSERTS, CLINK_DINNERS,
+                     CLINK_MAINS, CLINK_STARTERS, ChildrenTicket, DinnerTicket)
 
 
 class ChildrenTicketForm(forms.ModelForm):
@@ -62,3 +64,62 @@ class ChildrenTicketForm(forms.ModelForm):
         }
 
         return cls(data)
+
+
+class DinnerTicketForm(forms.ModelForm):
+
+    class Meta:
+        model = DinnerTicket
+        fields = [
+            'dinner',
+            'starter',
+            'main',
+            'dessert',
+        ]
+
+    @classmethod
+    def from_pending_order(cls, order):
+        assert order.payment_required()
+        unconfirmed_details = order.unconfirmed_details
+
+        data = {
+            'dinner': unconfirmed_details['dinner'],
+            'starter': unconfirmed_details['starter'],
+            'main': unconfirmed_details['main'],
+            'dessert': unconfirmed_details['dessert'],
+        }
+
+        if data['dinner'] in ['CD']:
+            return CityHallDinnerTicketForm(data)
+        else:
+            return ClinkDinnerTicketForm(data)
+
+    @classmethod
+    def from_item(cls, item):
+        assert not item.order.payment_required()
+
+        data = {
+            'dinner': item.item.dinner,
+            'starter': item.item.starter,
+            'main': item.item.main,
+            'dessert': item.item.dessert,
+        }
+
+        if item.item.dinner in ['CD']:
+            return CityHallDinnerTicketForm(data)
+        else:
+            return ClinkDinnerTicketForm(data)
+
+
+class ClinkDinnerTicketForm(DinnerTicketForm):
+    dinner = forms.ChoiceField(label='Event', choices=CLINK_DINNERS)
+    starter = forms.ChoiceField(choices=CLINK_STARTERS)
+    main = forms.ChoiceField(label='Main Course', help_text='gf: Gluten Free, v: vegetarian, vg: vegan', choices=CLINK_MAINS)
+    dessert = forms.ChoiceField(choices=CLINK_DESSERTS)
+
+
+class CityHallDinnerTicketForm(DinnerTicketForm):
+    dinner = forms.ChoiceField(label='Event', choices=CITY_HALL_DINNERS)
+    starter = forms.ChoiceField(choices=CITY_HALL_STARTERS)
+    main = forms.ChoiceField(label='Main Course', help_text='gf: Gluten Free, v: vegetarian, vg: vegan', choices=CITY_HALL_MAINS)
+    dessert = forms.ChoiceField(choices=CITY_HALL_DESSERTS)
--- extras/tests/test_actions.py
+++ extras/tests/test_actions.py
@@ -35,7 +35,7 @@ class CreatePendingChildrenTicketOrderTests(TestCase):
         self.assertEqual(order.billing_addr, 'Eadrax, Sirius Tau')
 
 
-class UpdatePendingOrderTests(TestCase):
+class UpdatePendingChildrenOrderTests(TestCase):
     def test_order_for_self_to_order_for_self(self):
         order = factories.create_pending_children_ticket_order()
         actions.update_pending_children_ticket_order(
@@ -72,3 +72,63 @@ class UpdatePendingOrderTests(TestCase):
         self.assertEqual(ticket.item.age, 10)
         self.assertEqual(ticket.item.accessibility_reqs, 'Occasionally breathe fire')
         self.assertEqual(ticket.item.dietary_reqs, 'Allergic to Welsh Cakes')
+
+
+class CreatePendingDinnerTicketOrderTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.alice = factories.create_user()
+
+    def test_order_for_self_individual(self):
+        order = actions.create_pending_dinner_ticket_order(
+            purchaser=self.alice,
+            billing_details={'name': 'Alice Apple', 'addr': 'Eadrax, Sirius Tau'},
+            unconfirmed_details={
+                'dinner': 'CD',
+                'starter': 'SESS',
+                'main': 'LTAC',
+                'dessert': 'ESB',
+            }
+        )
+
+        self.assertEqual(self.alice.orders.count(), 1)
+        self.assertIsNone(self.alice.get_ticket())
+
+        self.assertEqual(order.purchaser, self.alice)
+        self.assertEqual(order.status, 'pending')
+        self.assertEqual(order.invoice_number, None)
+        self.assertEqual(order.billing_name, 'Alice Apple')
+        self.assertEqual(order.billing_addr, 'Eadrax, Sirius Tau')
+
+
+class UpdatePendingDinnerOrderTests(TestCase):
+    def test_order_for_self_to_order_for_self(self):
+        order = factories.create_pending_dinner_ticket_order()
+        actions.update_pending_dinner_ticket_order(
+            order,
+            billing_details={'name': 'Alice Apple', 'addr': 'Eadrax, Sirius Tau'},
+            unconfirmed_details={
+                'dinner': 'CD',
+                'starter': 'SSSE',
+                'main': 'SSSS',
+                'dessert': 'ENB',
+            }
+        )
+
+        self.assertEqual(order.status, 'pending')
+        self.assertEqual(order.invoice_number, None)
+        self.assertEqual(order.billing_name, 'Alice Apple')
+        self.assertEqual(order.billing_addr, 'Eadrax, Sirius Tau')
+
+        [row] = order.all_order_rows()
+
+        self.assertEqual(row.cost_excl_vat, 30)
+        self.assertEqual(row.item_descr, "Dinner ticket")
+        self.assertEqual(row.item_descr_extra, '')
+
+        ticket = row.item
+        self.assertEqual(ticket.owner, order.purchaser)
+        self.assertEqual(ticket.item.dinner, 'CD')
+        self.assertEqual(ticket.item.starter, 'SSSE')
+        self.assertEqual(ticket.item.main, 'SSSS')
+        self.assertEqual(ticket.item.dessert, 'ENB')
